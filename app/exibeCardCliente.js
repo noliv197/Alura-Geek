a614b873bb699173de800d1e57769343f77a1da9
natalia <noliv197@gmail.com>
2022-10-06 17:02:54 -0300

    adição da funcionalidade do botao ver tudo

--- app/exibeCardCliente.js
+++ app/exibeCardCliente.js
@@ -1,15 +1,17 @@
 import {conectaAPI} from "./conecatAPI.js"
 const secao = document.querySelectorAll("[data-secao]") 
 
-function constroiCardCliente(src, alt, titulo, preco, id){ 
+function constroiCardCliente(src, alt, titulo, preco,categoria,id){ 
     const card = document.createElement("div"); 
     card.className = "card__produto"; 
     card.innerHTML = ` 
         <img src="${src}" alt="${alt}">
         <span class="texto__titulo">${titulo}</span>
-        <span class="texto__negrito">R$${preco}</span>
+        <span class="texto__negrito">R$${parseFloat(preco).toFixed(2)}</span>
         <a href="produto.html?id=${id}" class="texto__primario">Ver produto</a>
     ` 
+    card.dataset.id = id
+    card.dataset.categoria = categoria
     return card 
 }
 
@@ -17,16 +19,17 @@ function secaoFiltro(lista, secao){
     lista.forEach(elemento => {
         if (elemento.categoria == secao.id){
             secao.appendChild( 
-            constroiCardCliente(elemento.src,elemento.alt,elemento.titulo,elemento.preco,elemento.id)) 
+            constroiCardCliente(elemento.src,elemento.alt,elemento.titulo,elemento.preco,elemento.categoria,elemento.id)) 
         }})
 }
 
 async function exibeCardCliente(){ 
     try{
-    const listaAPI = await conectaAPI.getAPI() 
-    secao.forEach(div => secaoFiltro(listaAPI,div))
-    }catch(erro){
-     console.log(erro)
+        const listaAPI = await conectaAPI.getAPI() 
+        secao.forEach(div => secaoFiltro(listaAPI,div))
+    }
+    catch(erro){
+        console.log(erro)
     }
 } 
 exibeCardCliente()
