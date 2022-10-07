67e5cc595b67e573db03c0e715bc478edd001f24
natalia <noliv197@gmail.com>
2022-10-07 16:14:41 -0300

    adição de validação de inputs

---
+++ app/mensagemErro.js
@@ -0,0 +1,56 @@
+export class MensagemErro{
+    constructor(){
+        
+        this._tiposErro = [
+            'valueMissing',
+            'typeMismatch',
+            'patternMismatch',
+        ];
+        
+        this._mensagemErro = {
+            email: {
+                valueMissing: 'O campo de email não pode estar vazio.',
+                patternMismatch: 'Email inválido'
+            },
+            senha: {
+                valueMissing: 'O campo de senha não pode estar vazio.',
+                patternMismatch: 'Senha inválida'
+            },
+            nome:{
+                valueMissing: 'Campo não pode estar vazio '
+            },
+            mensagem:{
+                valueMissing: 'Campo não pode estar vazio '
+            },
+            url:{
+                valueMissing: 'Campo não pode estar vazio ',
+                typeMismatch: 'Url inválida'
+            },
+            categoria:{
+                valueMissing: 'Campo não pode estar vazio '
+            },
+            titulo:{
+                valueMissing: 'Campo não pode estar vazio '
+            },
+            preco:{
+                valueMissing: 'Campo não pode estar vazio ',
+                patternMismatch: 'Use somente números e "," para separar os decimais' 
+            },
+            descricao:{
+                valueMissing: 'Campo não pode estar vazio '
+            }
+        };
+
+    }
+
+    mostraErro(campo,tipoCampo){
+        let mensagem = '';
+        this._tiposErro.forEach(erro => {
+            if(campo.validity[erro]){
+                mensagem = this._mensagemErro[tipoCampo][erro];
+            }
+        });
+
+        return mensagem;
+    }   
+}
